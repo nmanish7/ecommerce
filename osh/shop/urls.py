61d361e9d65e3519fe9e4e4db5f3a42b26e4b815
nmanish7 <manishdas019@gmail.com>
2020-07-26 09:07:39 +0530

    Model and Migrated

--- osh/shop/urls.py
+++ osh/shop/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("",views.index,name="ShopHome"),
+urlpatterns = [
+    path("",views.index,name="ShopHome"),
+    path("about",views.about,name="About"),
+    path("contact",views.contact,name="ContactUs"),
+    path("tracker",views.track,name="TrackingStatus"),
+    path("search",views.search,name="Search"),
+    path("products",views.products,name="ProductViews"),
+    path("checkout",views.checkout,name="CheckOut"),
+
 
 ]
--- osh/shop/views.py
+++ osh/shop/views.py
@@ -5,3 +5,21 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,"shop/index.html")
+
+def about(request):
+    return HttpResponse("About Us")
+
+def contact(request):
+    return HttpResponse("Contact US")
+
+def track(request):
+    return HttpResponse("Track Your Products")
+
+def search(request):
+    return HttpResponse("Search")
+
+def products(request):
+    return HttpResponse("Products View")
+
+def checkout(request):
+    return HttpResponse("Checkout Page")
